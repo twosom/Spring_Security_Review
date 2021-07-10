fd5445467c157b468216b89410f424d68fb14c34
desirel <two_somang@icloud.com>
2021-07-10 22:50:04 +0900

    feat: 스프링 시큐리티 둘러보기

---
+++ server/basic-test/src/main/java/com/icloud/security/web/controller/SecurityMessage.java
@@ -0,0 +1,18 @@
+package com.icloud.security.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SecurityMessage {
+
+    private Authentication auth;
+    private String message;
+
+}
