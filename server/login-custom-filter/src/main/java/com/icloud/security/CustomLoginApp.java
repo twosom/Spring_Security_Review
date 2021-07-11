a91446bb3039dabb7ae1122a1387c0628711acfb
desirel <two_somang@icloud.com>
2021-07-11 19:54:22 +0900

    feat: Authentication 메카니즘 이해 복습 및 CustomLoginFilter 생성 및 적용

---
+++ server/login-custom-filter/src/main/java/com/icloud/security/CustomLoginApp.java
@@ -0,0 +1,12 @@
+package com.icloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomLoginApp.class, args);
+    }
+}
---
+++ server/login-custom-filter/src/main/java/com/icloud/security/teacher/TeacherAuthenticationProvider.java
@@ -0,0 +1,50 @@
+package com.icloud.security.teacher;
+
+import com.icloud.security.student.Student;
+import com.icloud.security.student.StudentAuthenticationToken;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class TeacherAuthenticationProvider implements AuthenticationProvider, InitializingBean {
+
+    private Map<String, Teacher> studentDB = new HashMap<>();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        if (studentDB.containsKey(token.getName())) {
+            Teacher teacher = studentDB.get(token.getName());
+            return TeacherAuthenticationToken.builder()
+                    .principal(teacher)
+                    .details(teacher.getUsername())
+                    .authenticated(true)
+                    .authorities(teacher.getRole())
+                    .build();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Set.of(
+                new Teacher("choi", "최선생", Set.of(new SimpleGrantedAuthority("ROLE_TEACHER")))
+        ).forEach(s -> {
+            studentDB.put(s.getId(), s);
+        });
+    }
+}
