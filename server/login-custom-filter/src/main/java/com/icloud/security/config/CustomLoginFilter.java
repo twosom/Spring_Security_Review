c0a70c7dbf8bfb687f65ab6e71d753b6bb2863c6
desirel <two_somang@icloud.com>
2021-07-11 19:54:22 +0900

    feat: Authentication 메카니즘 이해 복습 및 CustomLoginFilter 생성 및 적용

---
+++ server/login-custom-filter/src/main/java/com/icloud/security/config/CustomLoginFilter.java
@@ -0,0 +1,33 @@
+package com.icloud.security.config;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    public CustomLoginFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        String username = obtainUsername(request);
+        username = (username != null) ? username : "";
+        username = username.trim();
+        String password = obtainPassword(request);
+        password = (password != null) ? password : "";
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+        // Allow subclasses to set the "details" property
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    
+}
---
+++ server/login-custom-filter/src/main/java/com/icloud/security/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.icloud.security.config;
+
+
+import com.icloud.security.student.StudentAuthenticationProvider;
+import com.icloud.security.teacher.TeacherAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final StudentAuthenticationProvider studentAuthenticationProvider;
+    private final TeacherAuthenticationProvider teacherAuthenticationProvider;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(studentAuthenticationProvider)
+                .authenticationProvider(teacherAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        CustomLoginFilter customLoginFilter = new CustomLoginFilter(authenticationManager());
+        customLoginFilter.setAuthenticationFailureHandler((request, response, exception) -> {
+            response.sendRedirect("/login-error");
+        });
+
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/login-error").permitAll()
+                .anyRequest().authenticated()
+
+//            .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/", false)
+//                .failureUrl("/login-error")
+//                .permitAll()
+
+        .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied")
+        .and()
+                .addFilterAt(customLoginFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_TEACHER > ROLE_STUDENT");
+        return roleHierarchy;
+    }
+}
